893a34bc5473f495bc6bc9fdb644cbb32eb4df0e
Michael Garvin <mfjgarvin@gmail.com>
2013-09-06 00:14:57 -0400

    Just some starter code.

---
+++ FanLed/FanLed.ino
@@ -0,0 +1,27 @@
+//This is the beginning of my code. It's a start!
+int Xcounter;
+int Ycounter;
+//Note: If I want a certain X or Y value on startup, enter it above.
+int B1 = 1;
+int B2 = 2;
+int LED1 = 3;
+int LED2 = 4;
+int LED3 = 5;
+int LED4 = 6; //Change these!
+//And, some other stuff for LEDs...
+
+
+
+void setup () {
+  //Code that runs once on startup will go here.
+  pinMode(1, INPUT) //Button 1 input
+  pinMode(2, INPUT) //Button 2 input
+  pinMode(3, OUTPUT)
+  pinMode(4, OUTPUT)
+  pinMode(5, OUTPUT)
+  pinMode(6, OUTPUT)
+
+}
+void loop () {
+  //Code that needs to be run repeatedly.
+}
